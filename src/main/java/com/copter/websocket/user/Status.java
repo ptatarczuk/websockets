8b1241d7ad1bae441511436f4399ce5c82f71452
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2024-04-01 20:06:26 +0200

    Add user classes

---
+++ src/main/java/com/copter/websocket/user/Status.java
@@ -0,0 +1,5 @@
+package com.copter.websocket.user;
+
+public enum Status {
+    ONLINE, OFFLINE
+}
---
+++ src/main/java/com/copter/websocket/user/UserService.java
@@ -0,0 +1,29 @@
+package com.copter.websocket.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+    public void saveUser(User user) {
+        user.setStatus(Status.ONLINE);
+        repository.save(user);
+    }
+
+    public void disconnect(User user) {
+        var storedUser = repository.findById(user.getNickName())
+                .orElse(null);
+        if (storedUser != null) {
+            storedUser.setStatus(Status.OFFLINE);
+            repository.save(storedUser);
+        }
+    }
+
+    public List<User> findConnectedUsers() {
+        return repository.findAllByStatus(Status.ONLINE);
+    }
+
+}
